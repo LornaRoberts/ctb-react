7123de2c2a80dd0dbd1775d87b87cf99e8b02ca8
Lorna Roberts <lornaroberts99@gmail.com>
2020-12-08 18:41:04 +0000

    allows new spend to be sent

--- src/components/Spends/new.js
+++ src/components/Spends/new.js
@@ -1,76 +1,76 @@
 import React, { useState} from 'react';
-// import { useHistory} from 'react-router-dom';
 
-function NewSpend() {
+function NewSpend(){
 
-   const [spend, setSpend] = useState("");
-   function refreshPage() {
-    window.location.reload(false);
-    }
-
-    const handleSubmit = (evt) => {
-      console.log("handleSubmit is fired");
-      evt.preventDefault();
-      setSpend(spend);
-      // var retrievedObject = ####
-      newItem(JSON.parse(spend));
-      history.push('/spend');
-      refreshPage();
-
-
-        //alert(`Thank you ${name} for signing up. Please sign in.`)
-    }
+      const [date, setDate] = useState("");
+      const [spend, setSpend] = useState("");
+      const [cat, setCat] = useState("");
+      const [cost, setCost] = useState("");
 
 
 
-        async function newItem(date, item, cat, cost) {
-            const url = 'http://localhost:3080/spends'
-            await fetch(url, {
-              method: 'POST',
-              mode: 'cors',
-              body: JSON.stringify({dateSpent:dateSpent, itemSpent:itemSpent, itemCat:itemCat, itemCost:itemCost}),
-              headers: {'Content-Type': 'application/json'},
-             })
-            .then(function(resp) { return resp.text() }) // Convert data to json
-            .then(function(data) {
-                console.log('Success', data);
-                // window.localStorage.setItem('user', data); #####
-                // console.log(window.localStorage.getItem('user')); ####
-            })
-            .catch(function(error) {
-            });
-            }
 
+      function refreshPage() {
+        window.location.reload(false);
+      }
 
-    return (
-        <div>
-         <form>
-          <label>
-            Date:
-            <input type="date" name="date" />
-          </label><br></br>
-          <label>
-            Item:
-            <input type="text" name="name" />
-          </label>
-          <br></br>
-          <label>
-            Category:
-              <select id="categories" name="categories">
-                <option value="cat1">Entertainment</option>
-                <option value="cat2">Sports</option>
-                <option value="cat3">Food/Drink</option>
-                <option value="cat4">Other</option>
-              </select>
-          </label>
-          <br></br>
-          <label>
-            Cost:
-            <input type="number" name="cost" />
-          </label>
-          <br></br>
-          <input type="submit" value="Submit" />
-        </form>        </div>
+      const handleSubmit = (evt) => {
+          console.log("handleSubmit is fired");
+          evt.preventDefault();
+          setDate(date);
+          setSpend(spend);
+          setCat(cat);
+          setCost(cost);
+           postSpend(date, spend, cat, cost);
+           console.log("spend posted")
+           //refreshPage();
+      }
+
+      async function postSpend(date, spend, cat, cost) {
+          const url = 'http://localhost:3080/spends'
+          await fetch(url, {
+            method: 'POST',
+            mode: 'cors',
+            body: JSON.stringify({dateSpent: date, itemSpent: spend, itemCat: cat, itemCost: cost}),
+            headers: {'Content-Type': 'application/json'},
+           })
+          .then(function(resp) { return resp.json() }) // Convert data to json
+          .then(function(data) {
+            console.log('Success', data);
+          })
+          .catch(function(error) {
+          });
+          }
+
+          return (
+            <div>
+             <form >
+              <label>
+                Date:
+                <input type="date" name="date" value={date} onChange={e => setDate(e.target.value)} />
+              </label><br></br>
+              <label>
+                Item:
+                <input type="text" name="name" value={spend} onChange={e => setSpend(e.target.value)} />
+              </label>
+              <br></br>
+              <label>
+                Category:
+                  <select id="categories" name="categories" value={cat} onChange={e => setCat(e.target.value)} >
+                    <option value="cat1">Entertainment</option>
+                    <option value="cat2">Sports</option>
+                    <option value="cat3">Food/Drink</option>
+                    <option value="cat4">Other</option>
+                  </select>
+              </label>
+              <br></br>
+              <label>
+                Cost:
+                <input type="number" name="cost" value={cost} onChange={e => setCost(e.target.value)} />
+              </label>
+              <br></br>
+              <input type="submit" value="Submit" onClick={handleSubmit}/>
+            </form>        </div>
     );
 }
 
