c857a1926a56b766f6ba0c30e3fff05a98ae0ba1
Krzysztof Kasprzak <timmmc88@gmail.com>
2020-12-09 15:05:08 +0000

    front end good, now ready to test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import NewSpend from './components/Spends/new';
 import SingleSpend from './components/Spends/listofspends';
+import TSTP from './components/Spends/totalspendsthisperiod';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <TSTP />
         <NewSpend />
         <SingleSpend />
       </header>
---
+++ src/components/Spends/totalspendsthisperiod.js
@@ -0,0 +1,28 @@
+import React, {useState, useEffect} from 'react';
+
+
+
+function TSTP(){
+  const [total, setTotal] = useState('');
+  useEffect(()=>{
+        const url = 'http://localhost:3080/totals'
+         fetch(url, {
+          method: 'GET',
+          mode: 'cors',
+          headers: {'Content-Type': 'application/json'},
+         })
+        .then(function(resp) { return resp.json() }) // Convert data to json
+        .then(function(data) {
+          console.log('Success', data);
+          setTotal(data.totalSpendThisPeriod)
+        })
+        .catch(function(error) {
+        });
+        }
+  , [])
+
+  return (
+    <h1>The total spend this period: £{total}</h1>
+  );
+}
+export default TSTP;
