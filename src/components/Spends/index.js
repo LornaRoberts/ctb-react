35c4269dab326e229fc22b8d9f9f229612c22f39
Lorna Roberts <lornaroberts99@gmail.com>
2020-12-10 09:18:41 +0000

    tstp added

--- src/components/Spends/index.js
+++ src/components/Spends/index.js
@@ -1,9 +1,10 @@
 import ListOfSpends from './listofspends';
 import NewSpend from './new';
-
+import TSTP from './totalspendsthisperiod'; 
 function Spends() {
   return (
     <div className="App">
+    <TSTP /> 
      <NewSpend />
       <ListOfSpends />
     </div>
