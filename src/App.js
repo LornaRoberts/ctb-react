96db78f01844337e40d2f7261b80558dafaa3a3c
Melissa <mkneale1@hotmail.com>
2020-12-11 16:37:00 +0000

    App.js edited

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import './App.css';
-import NavBarComponent from './components/navBar';
-import Spends from './components/Spends/index';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './components/Home/index';
+import Routes from './Routes'
+import {BrowserRouter as Router} from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <NavBarComponent />
-        <Spends />
-        <Home />
+      <Router >
+        <Routes />
+        </ Router >
       </header>
     </div>
   );
