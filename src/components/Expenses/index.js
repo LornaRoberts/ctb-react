1827515c8138dcd4cc8159008cfe7af857b5d6dc
Ben Jones <benwjones76@outlook.com>
2020-12-13 12:41:31 +0000

    secured the other index pages

--- src/components/Expenses/index.js
+++ src/components/Expenses/index.js
@@ -1,7 +1,27 @@
+import React, { useEffect, useState } from 'react';
 import ListOfExpenses from './listofexpenses';
 import NewExpense from './new';
 import Tetp from './totalexpensesthisperiod';
 function Expenses() {
+
+  const [userObj, setUserObj] = useState();
+
+  useEffect(() => {
+      var retrievedObject = window.localStorage.getItem('userObj');
+      if (retrievedObject) {
+        setUserObj(retrievedObject);
+
+      }
+  }, [])    
+
+  if (!userObj) {
+    return (
+        <div> 
+          <p>You need to be signed in.</p>
+        </div>
+    )
+} else {
+
   return (
     <div className="App">
     <Tetp />
@@ -9,6 +29,7 @@ function Expenses() {
       <ListOfExpenses />
     </div>
   );
+  }
 }
 
 export default Expenses;
--- src/components/Spends/index.js
+++ src/components/Spends/index.js
@@ -1,7 +1,27 @@
+import React, { useEffect, useState } from 'react';
 import ListOfSpends from './listofspends';
 import NewSpend from './new';
 import Tstp from './totalspendsthisperiod';
+
 function Spends() {
+
+  const [userObj, setUserObj] = useState();
+
+    useEffect(() => {
+        var retrievedObject = window.localStorage.getItem('userObj');
+        if (retrievedObject) {
+          setUserObj(retrievedObject);
+
+        }
+    }, [])    
+
+    if (!userObj) {
+      return (
+          <div> 
+            <p>You need to be signed in.</p>
+          </div>
+      )
+  } else {
   return (
     <div className="App">
     <Tstp />
@@ -9,6 +29,7 @@ function Spends() {
       <ListOfSpends />
     </div>
   );
+  }
 }
 
 export default Spends;
