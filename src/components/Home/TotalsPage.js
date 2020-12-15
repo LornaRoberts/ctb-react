9ed60678a7847733b4929c00c57a07eb303f6ca9
Krzysztof Kasprzak <timmmc88@gmail.com>
2020-12-15 09:12:49 +0000

    mony left per day done

--- src/components/Home/TotalsPage.js
+++ src/components/Home/TotalsPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import TETP from '../Expenses/totalexpensesthisperiod'
 import TSTP from '../Spends/totalspendsthisperiod'
 import TML from './totalMoneyLeft'
+import MLPD from './moneyLeftPerDay'
 
 function TotalsPage (props) {
 
@@ -12,6 +13,7 @@ function TotalsPage (props) {
             <TSTP userId={props.userId}/>
             <TETP userId={props.userId}/>
             <TML userId={props.userId}/>
+            <MLPD userId={props.userId}/>
         </div>
     )
 }
---
+++ src/components/Home/moneyLeftPerDay.js
@@ -0,0 +1,61 @@
+import React, {useState, useEffect} from 'react';
+import './moneyleft.css';
+
+
+
+
+function MLPD(props){
+
+  const [salary, setSalary] = useState('');
+  const [expenses, setExpenses] = useState('');
+  const [spends, setSpends] = useState('');
+  const [days, setDays] = useState(1);
+
+  const getRemanningDays = function() {
+    const date = new Date();
+    setDays(new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate() - date.getDate() + 1);
+                      };
+  useEffect(()=>{
+    getRemanningDays();
+        const url = 'http://localhost:3080/totals/' + props.userId
+         fetch(url, {
+          method: 'GET',
+          mode: 'cors',
+          headers: {'Content-Type': 'application/json'},
+         })
+        .then(function(resp) { return resp.json() }) // Convert data to json
+        .then(function(data) {
+          console.log('Success', data);
+          setSalary(data.salary);
+          setSpends(data.totalSpendThisPeriod);
+        })
+        .catch(function(error) {
+        });
+
+        const url1 = 'http://localhost:3080/expenses/total/' + props.userId
+         fetch(url1, {
+          method: 'GET',
+          mode: 'cors',
+          headers: {'Content-Type': 'application/json'},
+         })
+        .then(function(resp) { return resp.json() }) // Convert data to json
+        .then(function(data) {
+          console.log('Success', data);
+          setExpenses(data.totalExpenseThisPeriod);
+        })
+        .catch(function(error) {
+        });
+        }
+  , [props.userId])
+
+
+
+
+
+  return (
+
+    <h1 className="TML" id="tetp">Money left per day: £<span id="tetp-value">{((salary - spends - parseFloat(expenses)) / days).toFixed(2)}</span></h1>
+
+  );
+}
+export default MLPD;
